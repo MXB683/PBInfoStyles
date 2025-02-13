c8d2c3ece3e4cb5f214ae39c12c25c49cd704e82
MXB683 <maximbogdan2011@gmail.com>
2025-02-13 15:59:50 +0200

    added button tooltips and hover effects

--- pbinfo/__all__/script.js
+++ pbinfo/__all__/script.js
@@ -122,6 +122,16 @@ chrome.storage.sync.get(
       document.querySelector("#navigare-li-probleme > a").innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm50.7-186.9L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path></svg>
       `;
+      document.querySelector("#navigare-li-probleme").innerHTML += `
+        <div class="pbs_tooltip" style="
+          position: absolute;
+          top: 4rem;
+          left: -2rem;
+          background: #333;
+          padding: 0.5rem 1rem;
+          border-radius: 1rem;
+        ">Probleme</div>
+      `;
 
       document.querySelector("#navigare-li-solutii > a").innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
@@ -130,9 +140,29 @@ chrome.storage.sync.get(
         .querySelector("#navigare-li-solutii > a")
         .classList.add("dropdown-toggle");
       document.querySelector("#navigare-li-solutii").style.margin = "0 1rem";
+      document.querySelector("#navigare-li-solutii").innerHTML += `
+        <div class="pbs_tooltip" style="
+          position: absolute;
+          top: 4rem;
+          left: -1.9rem;
+          background: #333;
+          padding: 0.5rem 1rem;
+          border-radius: 1rem;
+        ">Solutii</div>
+      `;
 
       document.querySelector("#navigare-li-resurse > a").innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336l24 0 0-64-24 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l48 0c13.3 0 24 10.7 24 24l0 88 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"></path></svg>
+      `;
+      document.querySelector("#navigare-li-resurse").innerHTML += `
+        <div class="pbs_tooltip" style="
+          position: absolute;
+          top: 4rem;
+          left: -2rem;
+          background: #333;
+          padding: 0.5rem 1rem;
+          border-radius: 1rem;
+        ">Resurse</div>
       `;
 
       if (
